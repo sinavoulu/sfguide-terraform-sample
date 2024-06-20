fddef96590561766af148adb73dc85f0f77ae9d4
sinavoulu <suresh.inavoulu@plexusworldwide.com>
2024-06-20 11:03:06 -0700

    Add Service User, Schema, Grants

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.87"
+    }
+  }
+}
+
 provider "snowflake" {
   alias = "security_admin"
   role  = "SECURITYADMIN"
